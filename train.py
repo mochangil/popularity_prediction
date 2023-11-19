11b77d1a412bf1c8a6576984d247ffbea16644c7
HYOJUNG08 <hjjung933@gmail.com>
2023-11-19 22:17:53 +0900

    feat: saving trained model

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import classification_report
-
+import joblib
 
 def run_model(model, alg_name):
    # build the model on training data
@@ -28,32 +28,33 @@ def run_model(model, alg_name):
    print("Accuracy on Test Set for {} = {:.2f}\n".format(alg_name,accuracy))
    print(Cr)
    print("{}: CrossVal Accuracy Mean: {:.2f} and Standard Deviation: {:.2f} \n".format(alg_name,scoresDT3.mean(), scoresDT3.std()))
+   joblib.dump(model, "./streamlit_app/model.pkl")
 
 
 if __name__=="__main__":
-    df = pd.read_csv('new_data.csv')
-    data = df.copy()
-    y = data['popularity_level']
-    X = data.drop(columns=['popularity','popularity_level'])
-    X_train, X_test, y_train, y_test = split(X, y, test_size = 0.25, random_state = 42)
+   df = pd.read_csv('new_data.csv')
+   data = df.copy()
+   y = data['popularity_level']
+   X = data.drop(columns=['popularity','popularity_level'])
+   X_train, X_test, y_train, y_test = split(X, y, test_size = 0.25, random_state = 42)
 
-    results = []
+   results = []
+   """
+   model = DecisionTreeClassifier()
+   run_model(model, "Decision Tree")
 
-    model = DecisionTreeClassifier()
-    run_model(model, "Decision Tree")
+   model = SVC(kernel='poly', degree=3, C=1)
+   run_model(model, "SVM Classifier")
 
-    model = SVC(kernel='poly', degree=3, C=1)
-    run_model(model, "SVM Classifier")
+   model = KNeighborsClassifier()
+   run_model(model, "Nearest Neighbors Classifier")
 
-    model = KNeighborsClassifier()
-    run_model(model, "Nearest Neighbors Classifier")
+   model = LogisticRegression(multi_class='multinomial' , solver='lbfgs', max_iter=100)
+   run_model(model, "Logistic Regression")
 
-    model = LogisticRegression(multi_class='multinomial' , solver='lbfgs', max_iter=100)
-    run_model(model, "Logistic Regression")
-
-    dt_b = DecisionTreeClassifier(max_depth=1, random_state=42)
-    model = AdaBoostClassifier(base_estimator=dt_b)
-    run_model(model, "Adaboost Classifier")
-
-    model = RandomForestClassifier(n_estimators=10)
-    run_model(model, "Random Forest")
+   dt_b = DecisionTreeClassifier(max_depth=1, random_state=42)
+   model = AdaBoostClassifier(base_estimator=dt_b)
+   run_model(model, "Adaboost Classifier")
+   """
+   model = RandomForestClassifier(n_estimators=10)
+   run_model(model, "Random Forest")
